8c6410828e2920fa1eca60d51fd683a85f7eed03
xAtilio00 <perprr4@gmail.com>
2022-10-06 00:08:21 -0300

    Agregación de Inicio y Desarrolladores

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Desarrolladores from './Component/Desarrolladores/Desarrolladores.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Inicio from './Component/Inicio/Inicio.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='App'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Inicio />}></Route>
+            <Route path='/Desarrolladores' element={<Desarrolladores />} />
+          </Routes>
+        </Router>
+        
     </div>
+    </>
   );
 }
 
