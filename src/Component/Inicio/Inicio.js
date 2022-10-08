3153eb641f078eaefb5c09dfba7cca3c5158bac1
EstebanEspada <fabri19espada@gmail.com>
2022-10-08 00:20:53 -0300

    actualizacion de inicio

--- src/Component/Inicio/Inicio.js
+++ src/Component/Inicio/Inicio.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
+import Img from 'react-bootstrap/image';
+import Portada from "../ImgAhorcado/00.png";
 
 function Inicio() {
   const navigate = useNavigate();
   return (
     <>
       <br></br>
+      <Img src={Portada} style={{width: '45%'}}/>
       <br></br>
-      <h1>Ahorcadito</h1>
       <br></br>
       <div className="text-center">
-        <Button>
+        <Button variant = 'secondary'>
           Jugar
         </Button>
         <br></br><br></br>
-        <Button onClick={() => navigate("/Desarrolladores")} variant='primary'>
+        <Button onClick={() => navigate("/Desarrolladores")} variant='secondary'>
           Desarrolladores
         </Button>
       </div>
